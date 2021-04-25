e14f33c8ed450670d97013795cd4d54996c013e4
Shen Jun Wu <shenfive@gmail.com>
2021-04-25 16:58:31 +0800

    Page2

---
+++ a425HelloPickerView/Page2ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  Page2ViewController.swift
+//  a425HelloPickerView
+//
+//  Created by 申潤五 on 2021/4/25.
+//
+
+import UIKit
+
+class Page2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("My VC")
+        
+    }
+
+
+}
